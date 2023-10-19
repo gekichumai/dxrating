11ed5f009d1075436ea4203f3b420d5ba764555e
GalvinGao <me@galvingao.com>
2023-10-19 00:53:31 -0400

    feat: add filter b15

--- apps/web/src/pages/SheetList.tsx
+++ apps/web/src/pages/SheetList.tsx
@@ -1,5 +1,6 @@
-import { Alert, TextField } from "@mui/material";
-import { FC, useState } from "react";
+import { VersionEnum } from "@gekichumai/dxdata";
+import { Alert, FormControlLabel, Switch, TextField } from "@mui/material";
+import { FC, useMemo, useState } from "react";
 import { SheetListContainer } from "../components/SheetListContainer";
 import { useFilteredSheets, useSheets } from "../songs";
 
@@ -7,6 +8,20 @@ export const SheetList: FC = () => {
   const { data: sheets } = useSheets();
   const [search, setSearch] = useState<string>("");
   const { results, elapsed } = useFilteredSheets(search);
+  const [showOnlyFestival, setShowOnlyFestival] = useState<boolean>(false);
+
+  const filteredResults = useMemo(() => {
+    if (showOnlyFestival) {
+      return results
+        .filter(
+          (sheet) =>
+            sheet.item.version === VersionEnum.FESTIVAL ||
+            sheet.item.version === VersionEnum.FESTIVALPLUS,
+        )
+        .sort((a, b) => a.item.internalLevelValue - b.item.internalLevelValue);
+    }
+    return results;
+  }, [results, showOnlyFestival]);
 
   return (
     <div className="flex-container pb-global">
@@ -18,12 +33,22 @@ export const SheetList: FC = () => {
         onChange={(e) => setSearch(e.target.value)}
       />
 
+      <FormControlLabel
+        control={
+          <Switch
+            checked={showOnlyFestival}
+            onChange={(e) => setShowOnlyFestival(e.target.checked)}
+          />
+        }
+        label="Filter B15: Show only FESTiVAL/FESTiVAL+ charts and sort by level (ascending)"
+      />
+
       <Alert severity="info" className="text-sm !rounded-full shadow-lg">
-        Found {results.length} results out of {sheets?.length} records in{" "}
-        {elapsed.toFixed(1)}ms
+        Found {filteredResults.length} results out of {sheets?.length} records
+        in {elapsed.toFixed(1)}ms
       </Alert>
 
-      <SheetListContainer sheets={results} />
+      <SheetListContainer sheets={filteredResults} />
     </div>
   );
 };
