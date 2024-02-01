9cd036a3edeab9f507f0df05f9e21929637b845f
GalvinGao <me@galvingao.com>
2024-02-02 04:39:49 +0800

    perf: use direct dom manip on OverscrollBackgroundFiller

--- apps/web/src/components/global/OverscrollBackgroundFiller.tsx
+++ apps/web/src/components/global/OverscrollBackgroundFiller.tsx
@@ -1,18 +1,33 @@
-import { useWindowScroll } from "react-use";
+import { useEffect, useRef } from "react";
 import { useVersionTheme } from "../../utils/useVersionTheme";
 
 const HEIGHT = 16 * 4;
 const DETECT_HEIGHT = 16 * 16;
 
 export const OverscrollBackgroundFiller = () => {
-  const scroll = useWindowScroll();
+  // const scroll = useWindowScroll();
+  const ref = useRef<HTMLDivElement>(null);
   const versionTheme = useVersionTheme();
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (ref.current) {
+        ref.current.style.height = `${
+          window.scrollY < DETECT_HEIGHT ? -window.scrollY + HEIGHT : 0
+        }px`;
+      }
+    };
+
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [ref]);
 
   return (
     <div
+      ref={ref}
       className="fixed top-0 left-0 right-0 w-full h-full pointer-events-none"
       style={{
-        height: `${scroll.y < DETECT_HEIGHT ? -scroll.y + HEIGHT : 0}px`,
+        height: 0,
         background: versionTheme.accentColor,
       }}
     />
