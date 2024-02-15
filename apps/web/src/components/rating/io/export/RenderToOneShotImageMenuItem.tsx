457ebac4277efa06271acd3c1f388a6b6c49cf51
GalvinGao <me@galvingao.com>
2024-02-16 01:04:27 +0800

    refactor: remove oneshot for now

--- apps/web/src/components/rating/io/export/RenderToOneShotImageMenuItem.tsx
+++ apps/web/src/components/rating/io/export/RenderToOneShotImageMenuItem.tsx
@@ -1,38 +1,14 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  ListItemIcon,
-  ListItemText,
-  MenuItem,
-} from "@mui/material";
-import { FC, Suspense, lazy, useState } from "react";
+import { ListItemIcon, ListItemText, MenuItem } from "@mui/material";
+import { FC } from "react";
 import IconMdiImage from "~icons/mdi/image";
 import { Entry } from "../../../../pages/RatingCalculator";
 
-const OneShotImage = lazy(() => import("../../../oneshot/OneShotImage"));
-
 export const RenderToOneShotImageMenuItem: FC<{
   calculatedEntries: Entry[];
-}> = ({ calculatedEntries }) => {
-  const [open, setOpen] = useState(false);
+}> = () => {
   return (
     <>
-      <Dialog open={open} onClose={() => setOpen(false)} maxWidth="md">
-        <DialogTitle>Export as OneShot Image</DialogTitle>
-        <DialogContent>
-          <Suspense fallback={<div>Loading...</div>}>
-            <OneShotImage calculatedEntries={calculatedEntries} />
-          </Suspense>
-        </DialogContent>
-      </Dialog>
-
-      <MenuItem
-        onClick={() => {
-          setOpen(true);
-        }}
-        disabled
-      >
+      <MenuItem disabled>
         <ListItemIcon>
           <IconMdiImage />
         </ListItemIcon>
