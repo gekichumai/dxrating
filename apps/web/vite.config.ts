615ca36958af405ed53b2e803a9efd92f5576493
GalvinGao <me@galvingao.com>
2024-04-13 15:22:58 +0800

    chore: improve sentry integration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import UnoCSS from "unocss/vite";
 import Icons from "unplugin-icons/vite";
@@ -9,6 +10,14 @@ export default defineConfig({
     react(),
     UnoCSS(),
     Icons({ compiler: "jsx", jsx: "react", autoInstall: true }),
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+
+      // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      reactComponentAnnotation: { enabled: true },
+    }),
   ],
   build: {
     rollupOptions: {
