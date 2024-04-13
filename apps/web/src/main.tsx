97d72baf4bef6623bc3b8a26003fc4855cd89c47
GalvinGao <me@galvingao.com>
2024-04-13 16:03:17 +0800

    chore: improve supabase integration

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -14,91 +14,92 @@ import { initReactI18next } from "react-i18next";
 import { i18nResources } from "./locales/locales";
 
 import * as Sentry from "@sentry/react";
+import { browserTracingIntegration } from "@sentry/react";
 import { SupabaseIntegration } from "@supabase/sentry-js-integration";
+import { SupabaseClient } from "@supabase/supabase-js";
 import { Geiger } from "react-geiger";
 import { SideEffector } from "./components/global/SideEffector";
 import { RatingCalculatorContextProvider } from "./models/RatingCalculatorContext";
-import { supabase } from "./models/supabase";
 import { BUNDLE } from "./utils/bundle";
 
-if (import.meta.env.PROD) {
-  Sentry.init({
-    dsn: "https://1e929f3c3b929a213436e3c4dff57140@o4506648698683392.ingest.sentry.io/4506648709627904",
-    tunnel: "https://derrakuma.dxrating.net/functions/v1/science-tunnel",
-    release: "dxrating@" + (BUNDLE.version ?? "unknown"),
-    integrations: [
-      new Sentry.BrowserTracing({
-        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: [
-          "localhost",
-          /^https?:\/\/.*\.dxrating\.net/,
-          /^https?:\/\/dxrating\.net/,
-          /^https?:\/\/dxrating\.imgg\.dev/,
-        ],
-      }),
-      new SupabaseIntegration(supabase, {
-        tracing: true,
-        breadcrumbs: true,
-        errors: true,
-      }),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 0.2,
-    ignoreErrors: [
-      /// START: https://gist.github.com/Chocksy/e9b2cdd4afc2aadc7989762c4b8b495a
-      "top.GLOBALS",
-      // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error.html
-      "originalCreateNotification",
-      "canvas.contentDocument",
-      "MyApp_RemoveAllHighlights",
-      "http://tt.epicplay.com",
-      "Can't find variable: ZiteReader",
-      "jigsaw is not defined",
-      "ComboSearch is not defined",
-      "http://loading.retry.widdit.com/",
-      "atomicFindClose",
-      // Facebook borked
-      "fb_xd_fragment",
-      // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to
-      // reduce this. (thanks @acdha)
-      // See http://stackoverflow.com/questions/4113268
-      "bmi_SafeAddOnload",
-      "EBCallBackMessageReceived",
-      // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
-      "conduitPage",
-      // Avast extension error
-      "_avast_submit",
-      /// END
+Sentry.init({
+  dsn: "https://1e929f3c3b929a213436e3c4dff57140@o4506648698683392.ingest.sentry.io/4506648709627904",
+  tunnel: "https://derrakuma.dxrating.net/functions/v1/science-tunnel",
+  release: "dxrating@" + (BUNDLE.version ?? "unknown"),
+  enabled: import.meta.env.PROD,
+  integrations: [
+    browserTracingIntegration({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: [
+        "localhost",
+        /^https?:\/\/.*\.dxrating\.net/,
+        /^https?:\/\/dxrating\.net/,
+        /^https?:\/\/dxrating\.imgg\.dev/,
+      ],
+      shouldCreateSpanForRequest: (url) => {
+        return !url.startsWith(`${import.meta.env.VITE_SUPABASE_URL}/rest`);
+      },
+    }),
+    new SupabaseIntegration(SupabaseClient, {
+      tracing: true,
+      breadcrumbs: true,
+      errors: true,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 0.2,
+  ignoreErrors: [
+    /// START: https://gist.github.com/Chocksy/e9b2cdd4afc2aadc7989762c4b8b495a
+    "top.GLOBALS",
+    // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error.html
+    "originalCreateNotification",
+    "canvas.contentDocument",
+    "MyApp_RemoveAllHighlights",
+    "http://tt.epicplay.com",
+    "Can't find variable: ZiteReader",
+    "jigsaw is not defined",
+    "ComboSearch is not defined",
+    "http://loading.retry.widdit.com/",
+    "atomicFindClose",
+    // Facebook borked
+    "fb_xd_fragment",
+    // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to
+    // reduce this. (thanks @acdha)
+    // See http://stackoverflow.com/questions/4113268
+    "bmi_SafeAddOnload",
+    "EBCallBackMessageReceived",
+    // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
+    "conduitPage",
+    // Avast extension error
+    "_avast_submit",
+    /// END
 
-      /// START: Customized ones
-      "vivoNewsDetailPage",
-      "removeAD",
-      "ucbrowser",
-      /// END
-    ],
-    denyUrls: [
-      // Google Adsense
-      /pagead\/js/i,
-      // Facebook flakiness
-      /graph\.facebook\.com/i,
-      // Facebook blocked
-      /connect\.facebook\.net\/en_US\/all\.js/i,
-      // Woopra flakiness
-      /eatdifferent\.com\.woopra-ns\.com/i,
-      /static\.woopra\.com\/js\/woopra\.js/i,
-      // Chrome extensions
-      /extensions\//i,
-      /^chrome:\/\//i,
-      // Other plugins
-      /127\.0\.0\.1:4001\/isrunning/i, // Cacaoweb
-      /webappstoolbarba\.texthelp\.com\//i,
-      /metrics\.itunes\.apple\.com\.edgesuite\.net\//i,
-    ],
-    ignoreTransactions: ["clarity.ms/collect"],
-  });
-} else {
-  console.log("Not in production, skipping Sentry initialization");
-}
+    /// START: Customized ones
+    "vivoNewsDetailPage",
+    "removeAD",
+    "ucbrowser",
+    /// END
+  ],
+  denyUrls: [
+    // Google Adsense
+    /pagead\/js/i,
+    // Facebook flakiness
+    /graph\.facebook\.com/i,
+    // Facebook blocked
+    /connect\.facebook\.net\/en_US\/all\.js/i,
+    // Woopra flakiness
+    /eatdifferent\.com\.woopra-ns\.com/i,
+    /static\.woopra\.com\/js\/woopra\.js/i,
+    // Chrome extensions
+    /extensions\//i,
+    /^chrome:\/\//i,
+    // Other plugins
+    /127\.0\.0\.1:4001\/isrunning/i, // Cacaoweb
+    /webappstoolbarba\.texthelp\.com\//i,
+    /metrics\.itunes\.apple\.com\.edgesuite\.net\//i,
+  ],
+  ignoreTransactions: ["clarity.ms/collect"],
+});
 
 i18n
   .use(initReactI18next)
