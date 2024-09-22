bcd24b07cb687433e75844717152aaf4da4241a2
AnchorCat <AnchorCat@bytedance.com>
2024-09-22 15:28:14 +0800

    feature/add_d_b50

--- apps/web/src/components/rating/io/ImportMenu.tsx
+++ apps/web/src/components/rating/io/ImportMenu.tsx
@@ -7,6 +7,7 @@ import { PlayEntry } from "../RatingCalculatorAddEntryForm";
 import { ImportFromAquaSQLiteListItem } from "./import/ImportFromAquaSQLiteListItem";
 import { ImportFromJSONButtonListItem } from "./import/ImportFromJSONButtonListItem";
 import { ImportFromNETRecordsListItem } from "./import/ImportFromNETRecordsListItem";
+import { ImportFromDivingFishButtonListItem } from "./import/ImportFromDivingFishButtonListItem";
 
 export const ImportMenu: FC<{
   modifyEntries: ListActions<PlayEntry>;
@@ -55,6 +56,10 @@ export const ImportMenu: FC<{
           onClose={handleClose}
         />
         <ImportFromAquaSQLiteListItem
+          modifyEntries={modifyEntries}
+          onClose={handleClose}
+        />
+        <ImportFromDivingFishButtonListItem
           modifyEntries={modifyEntries}
           onClose={handleClose}
         />
